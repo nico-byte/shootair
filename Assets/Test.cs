4cb5651005c196aa1c6ac51d42f9fa1a1b193647
ntrprs <robin.o.krn@gmail.com>
2024-02-15 19:00:54 +0100

    Test commit
    Test

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string msg = "Test";
+        Console.Write(msg)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
