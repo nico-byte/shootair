03ce44e41b8768376aacd2e77ceaddbcbb00c4fd
Nico Fuchs <nicofuchs653@outlook.de>
2024-02-23 15:15:05 +0100

    refactored Agent.cs

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -3,45 +3,33 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
-using Unity.VisualScripting;
-using Unity.Sentis.Layers;
-using JetBrains.Annotations;
-using System.Runtime.CompilerServices;
 
 public class ShootairAgent : Agent
 {
     Rigidbody2D rBody;
-    public float moveSpeed = 5f;
-
-    public float turnSpeed = 360f;
-
+    AgentSettings agentSettings;
     // Gun properties
-    public bool autoShoot = false;
     public GameObject bulletPrefab;
     public GameObject enemyPrefab;
     public Transform firingPoint;
-    public float fireRate = 0.5f;
     public int numberOfTargets = 8;
-
-    public float fireTimer;
-    private int lastRewardExp;
     private int enemiesInit;
     public Transform Enemy;
-    public Transform FiringPoint;
-    public Transform NorthWall;
-    public Transform SouthWall;
-    public Transform WestWall;
-    public Transform EastWall;
     public Transform Bullet;
     public int warmup = 50000;
-    private bool hasShot = false;
+    
     public override void Initialize()
     {
         base.Initialize();
         rBody = GetComponent<Rigidbody2D>();
-        // Hides the cursor...
+        agentSettings = FindObjectOfType<AgentSettings>();
     }
 
+    void Update()
+    {
+         Cursor.visible = false;
+         Cursor.lockState = CursorLockMode.Locked;
+    }
     
     public override void OnEpisodeBegin()
     {
@@ -49,10 +37,7 @@ public class ShootairAgent : Agent
         {
             MaxStep = 1000;
         } else MaxStep = 10000;
-
-        hasShot = false;
         
-        lastRewardExp = 0;
         enemiesInit = numberOfTargets * 2;
         
         this.rBody.angularVelocity = 0;
@@ -65,7 +50,7 @@ public class ShootairAgent : Agent
             if(obj.transform.name == "Enemy(Clone)"){
                 Destroy(obj);
             }
-}
+        }
 
         // Move the target to a new spot
         for (int i = 0; i <= numberOfTargets; i++)
@@ -79,63 +64,27 @@ public class ShootairAgent : Agent
         }
     }
 
-    void Update()
-    {
-         Cursor.visible = false;
-         Cursor.lockState = CursorLockMode.Locked;
-    }
-
     public override void CollectObservations(VectorSensor sensor)
     {
-        // Target and Agent positions
-        sensor.AddObservation(Enemy.localPosition);
+        // Agent position
         sensor.AddObservation(this.transform.localPosition);
         
         // Agent rotation
         sensor.AddObservation(this.transform.rotation);
 
         // Firing point position and rotation
-        sensor.AddObservation(FiringPoint.position);
-        sensor.AddObservation(FiringPoint.rotation);
-
-        // distance to enemies
-        float distanceToEnemies = Vector2.Distance(this.transform.localPosition, Enemy.localPosition);
-        sensor.AddObservation(distanceToEnemies);
-
-        // Walls
-        float distanceToNorthWall = Vector2.Distance(this.transform.localPosition, NorthWall.localPosition);
-        float distanceToSouthWall = Vector2.Distance(this.transform.localPosition, SouthWall.localPosition);
-        float distanceToWestWall = Vector2.Distance(this.transform.localPosition, WestWall.localPosition);
-        float distanceToEasthWall = Vector2.Distance(this.transform.localPosition, EastWall.localPosition);
-        sensor.AddObservation(distanceToNorthWall);
-        sensor.AddObservation(distanceToSouthWall);
-        sensor.AddObservation(distanceToWestWall);
-        sensor.AddObservation(distanceToEasthWall);
-
-        // Bullet
-        // sensor.AddObservation(Bullet.position);
-        // sensor.AddObservation(Bullet.rotation);
-        float distanceBulletEnemy = Vector2.Distance(Bullet.position, Enemy.position);
-        sensor.AddObservation(distanceBulletEnemy);
+        sensor.AddObservation(firingPoint.position);
+        sensor.AddObservation(firingPoint.rotation);
 
         // Agent velocity
         sensor.AddObservation(rBody.velocity.x);
         sensor.AddObservation(rBody.velocity.y);
     }
 
-    /// Moves the agent according to the selected action.
-    /// </summary>
-    public override void OnActionReceived(ActionBuffers actionBuffers)
+    public void MoveAgent(ActionBuffers actionBuffers)
     {
-        if (GameObject.Find("Enemy(Clone)") == null)
-        {
-            AddReward(1f-GetCumulativeReward());
-            Debug.Log("Episode " + CompletedEpisodes + " completed with reward: " + GetCumulativeReward());
-            EndEpisode();
-        }
-        
-        // Convert the first action to forward movement
         float forwardAmount = 0f;       
+        // Convert first action to moving forward or backwards
         if (actionBuffers.DiscreteActions[0] == 1f)
         {
             forwardAmount = 1f;
@@ -148,77 +97,40 @@ public class ShootairAgent : Agent
         // Convert the second action to turning left or right
         float turnAmount = 0f;
         if (actionBuffers.DiscreteActions[1] == 1f)
-        {;
-                hasShot = true;
+        {
             turnAmount = -1f;
         }
         else if (actionBuffers.DiscreteActions[1] == 2f)
         {
             turnAmount = 1f;
         }
-        if ((actionBuffers.ContinuousActions[1] == 1 && fireTimer <= 0f) || (autoShoot && fireTimer <= 0f))
+        if ((actionBuffers.ContinuousActions[1] == 1 && agentSettings.fireTimer <= 0f) || (agentSettings.autoShoot && agentSettings.fireTimer <= 0f))
         {
             Shoot();
-            if (!autoShoot)
-            {
-                AddReward(0.3f/(MaxStep*fireRate));
-                hasShot = true;
-                Debug.Log("Shoot!");
-            }
-            fireTimer = fireRate;
+            agentSettings.fireTimer = agentSettings.fireRate;
         } else
         {
-            fireTimer -= Time.deltaTime;
+            agentSettings.fireTimer -= Time.deltaTime;
         }
 
-        // Apply movement
-        rBody.MovePosition(transform.position + transform.up * forwardAmount * moveSpeed * Time.fixedDeltaTime + transform.right * turnAmount * moveSpeed * Time.fixedDeltaTime);
-        transform.Rotate(transform.forward * -actionBuffers.ContinuousActions[0] * turnSpeed * Time.fixedDeltaTime);
-
-        // Apply a tiny negative reward every step to encourage action
-        if (MaxStep > 0 && fireTimer < -5f) AddReward(-1f / MaxStep);
-
-        float distanceBulletEnemy = Vector2.Distance(Bullet.position, Enemy.position);
-        if (distanceBulletEnemy <= 1.64f && CompletedEpisodes < warmup) AddReward(0.2f / (fireRate*MaxStep));
-
-        int leftEnemies = GameObject.FindGameObjectsWithTag("target").Length;
-        int rewardExp = enemiesInit - leftEnemies;
-        
-
-        if (rewardExp > lastRewardExp)
-        {
-            AddReward(0.5f / enemiesInit);
-            lastRewardExp = rewardExp;
-        }
-
-        if (StepCount == MaxStep)
-        {
-            AddReward(-0.3f);
-            Debug.Log("Episode " + CompletedEpisodes + " expired with reward: " + GetCumulativeReward());
-            EndEpisode();
-        }
+        // Apply movement and shoot
+        rBody.MovePosition(transform.position + transform.up * forwardAmount * agentSettings.moveSpeed * Time.fixedDeltaTime + transform.right * turnAmount * agentSettings.moveSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.forward * -actionBuffers.ContinuousActions[0] * agentSettings.turnSpeed * Time.fixedDeltaTime);
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    /// Moves the agent according to the selected action.
+    /// </summary>
+    public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        if (collision.transform.tag == "target")
+        if (GameObject.Find("Enemy(Clone)") == null)
         {
-            if (hasShot) AddReward(-0.3f);
-            else AddReward(-1f-GetCumulativeReward());
-            Debug.Log("Episode " + CompletedEpisodes + " canceled with reward: " + GetCumulativeReward());
+            AddReward(1f-GetCumulativeReward());
+            Debug.Log("Episode " + CompletedEpisodes + " completed with reward: " + GetCumulativeReward());
             EndEpisode();
         }
-        if (collision.transform.tag == "wall")
-        {
-            if (CompletedEpisodes < warmup) 
-            {
-                if (hasShot) AddReward(-0.3f);
-                else AddReward(-1f-GetCumulativeReward());
-                Debug.Log("Episode " + CompletedEpisodes + " canceled with reward: " + GetCumulativeReward());
-                EndEpisode();
-            } else AddReward(-1f/MaxStep);
-        }
+        MoveAgent(actionBuffers);
     }
+
     private void Shoot()
     {
         Instantiate(bulletPrefab, firingPoint.position, firingPoint.rotation);
@@ -228,7 +140,7 @@ public class ShootairAgent : Agent
     {
         int forwardAction = 0;
         int turnAction = 0;
-        int shootAction = 0;
+        
         if (Input.GetKey(KeyCode.W))
         {
             // move forward
@@ -239,6 +151,7 @@ public class ShootairAgent : Agent
             // move forward
             forwardAction = 2;
         }
+        
         if (Input.GetKey(KeyCode.A))
         {
             // turn left
@@ -249,17 +162,12 @@ public class ShootairAgent : Agent
             // turn right
             turnAction = 2;
         }
-        if (Input.GetKey(KeyCode.Space))
-        {
-            // turn right
-            shootAction = 1;
-        }
 
         // Put the actions into the array
         actionsOut.DiscreteActions.Array[0] = forwardAction;
         actionsOut.DiscreteActions.Array[1] = turnAction;
-        // actionsOut.DiscreteActions.Array[2] = shootAction;
+        
         actionsOut.ContinuousActions.Array[0] = Input.GetAxisRaw("Mouse X");
-        actionsOut.ContinuousActions.Array[1] = shootAction;
+        actionsOut.ContinuousActions.Array[1] = Input.GetKey(KeyCode.Space) ? 1f : 0f;
     }
 }
