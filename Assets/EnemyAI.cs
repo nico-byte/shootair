3928d3b4452df7df0bdd90462063fbc8f8486f46
Nico Fuchs <nicofuchs653@outlook.de>
2024-03-20 21:34:14 +0100

    added partial waypoint system - results in stack overflow

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using Unity.MLAgents;
+using Unity.Sentis.Layers;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,9 +11,16 @@ public class EnemyAI : MonoBehaviour {
     private RaycastHit2D ray2d;
     private Transform target;
 	
-	Vector3 direction;
+	UnityEngine.Vector3 direction;
 	private int currentTarget;
-	private Transform[] waypoints = null;
+    private Transform[] currentPath = null;
+	private Transform[] mainPath = null;
+    private Transform[] secPath1 = null;
+    private Transform[] secPath2 = null;
+    private Transform[] secPath3 = null;
+    private Transform[] secPath4 = null;
+    private List<Transform[]> paths = null;
+
 	public float speed;
 
 	private Animator anim;
@@ -41,7 +50,15 @@ public class EnemyAI : MonoBehaviour {
         Transform point7 = GameObject.Find("p7").transform;
         Transform point8 = GameObject.Find("p8").transform;
         Transform point9 = GameObject.Find("p9").transform;
-        waypoints = new Transform[9] {
+        Transform point10 = GameObject.Find("p10").transform;
+        Transform point11 = GameObject.Find("p11").transform;
+        Transform point12 = GameObject.Find("p12").transform;
+        Transform point13 = GameObject.Find("p13").transform;
+        Transform point14 = GameObject.Find("p14").transform;
+		Transform point15 = GameObject.Find("p15").transform;
+        Transform point22 = GameObject.Find("p22").transform;
+        Transform point16 = GameObject.Find("p16").transform;
+        mainPath = new Transform[16] {
             point1,
             point2,
             point3,
@@ -50,7 +67,49 @@ public class EnemyAI : MonoBehaviour {
 			point6,
             point7,
             point8,
-            point9
+            point9,
+            point10,
+            point11,
+            point12,
+            point13,
+            point14,
+            point15,
+            point16
+        };
+
+        Transform point17 = GameObject.Find("p17").transform;
+        Transform point19 = GameObject.Find("p19").transform;
+        secPath1 = new Transform[2] {
+            point17,
+            point19
+        };
+
+        Transform point20 = GameObject.Find("p20").transform;
+        Transform point18 = GameObject.Find("p18").transform;
+        secPath2 = new Transform[2] {
+            point20,
+            point18
+        };
+
+        Transform point21 = GameObject.Find("p21").transform;
+        secPath3 = new Transform[3] {
+            point20,
+            point18,
+            point21
+        };
+
+        secPath3 = new Transform[3] {
+            point20,
+            point18,
+            point21
+        };
+
+        paths = new List<Transform[]> {
+            mainPath,
+            secPath1,
+            secPath2,
+            secPath3,
+            secPath4
         };
 	}
 	
@@ -74,23 +133,23 @@ public class EnemyAI : MonoBehaviour {
         {
             if (!chasing)
             {
-                direction = waypoints[currentTarget].position - transform.position;
+                direction = mainPath[currentTarget].position - transform.position;
                 rotateEnemy();
             }
-            Vector2 rayDir = new Vector2(direction.x, direction.y);
-            ray2d = Physics2D.Raycast(rBody.position, new Vector2(direction.x, direction.y), 4f);
+            UnityEngine.Vector2 rayDir = new UnityEngine.Vector2(direction.x, direction.y);
+            ray2d = Physics2D.Raycast(rBody.position, new UnityEngine.Vector2(direction.x, direction.y), 4f);
             if (ray2d.collider.tag == "obstacle")
                 {
                     if (ray2d.distance <= 2f)
                     {
-                        rayDir = Vector2.Perpendicular(rayDir);
-                        rBody.MovePosition(Vector2.MoveTowards(rBody.position, rBody.position + rayDir, speed * Time.fixedDeltaTime));
+                        rayDir = UnityEngine.Vector2.Perpendicular(rayDir);
+                        rBody.MovePosition(UnityEngine.Vector2.MoveTowards(rBody.position, rBody.position + rayDir, speed * Time.fixedDeltaTime));
                         Debug.DrawRay(rBody.position, rayDir);
                     }
                 }
                 else
                 {
-                    rBody.MovePosition(Vector2.MoveTowards(rBody.position, rBody.position + rayDir, speed * Time.fixedDeltaTime));
+                    rBody.MovePosition(UnityEngine.Vector2.MoveTowards(rBody.position, rBody.position + rayDir, speed * Time.fixedDeltaTime));
                     Debug.DrawRay(rBody.position, rayDir);
                 }
         }
@@ -100,7 +159,7 @@ public class EnemyAI : MonoBehaviour {
 	private void FixedUpdate()
     {
         // Give the values to the FSM (animator)
-        distanceFromTarget = Vector3.Distance(waypoints[currentTarget].position, transform.position);
+        distanceFromTarget = UnityEngine.Vector3.Distance(mainPath[currentTarget].position, transform.position);
         anim.SetFloat("distanceFromWaypoint", distanceFromTarget);
         anim.SetBool("playerInSight", inViewCone);
  
@@ -110,23 +169,40 @@ public class EnemyAI : MonoBehaviour {
     {
         // Pick a random waypoint 
         // But make sure it is not the same as the last one
-        int lastWaypoint = currentTarget;
-        int nextPoint = -1;
- 
-        do
+        if (currentPath == null)
         {
-           nextPoint =  Random.Range(0, waypoints.Length - 1);
-           if (nextPoint == 0 && (lastWaypoint == 3 || lastWaypoint == 4 || lastWaypoint == 5 || lastWaypoint == 6))
-           {
+            currentPath = mainPath;
+            int idxMinDistance = 100;
+            float minDistance = 100;
+            for (int i = 0; i<=mainPath.Length-1; i++)
+            {
+                if (UnityEngine.Vector2.Distance(transform.position, mainPath[i].position) < minDistance)
+                {
+                    idxMinDistance = i;
+                }
+            }
+            currentTarget = idxMinDistance;
+
+            direction = mainPath[idxMinDistance].position - transform.position;
+            rotateEnemy();
+        }
+
+        int nextPoint = -1;
+
+        bool goMainPath = currentTarget != 11 || currentTarget != 13 || currentTarget != 14 || currentTarget != 16 || currentTarget != 22 || currentTarget != 5 || currentTarget != 3 || currentTarget != 6 || currentTarget != 8;
+ 
+        bool idxIn = currentTarget <= currentPath.Length-1;
+        do
+        {   
+            nextPoint = idxIn ? currentTarget+1 : 0;
             SetNextPoint();
-           }
         }
         while (nextPoint == currentTarget);
  
         currentTarget = nextPoint;
  
         // Load the direction of the next waypoint
-        direction = waypoints[currentTarget].position - transform.position;
+        direction = currentPath[currentTarget].position - transform.position;
         rotateEnemy();
     }
  
@@ -145,7 +221,7 @@ public class EnemyAI : MonoBehaviour {
     private void rotateEnemy()
     {
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+        transform.rotation = UnityEngine.Quaternion.Euler(new UnityEngine.Vector3(0, 0, angle - 90));
         direction = direction.normalized;
     }
  
