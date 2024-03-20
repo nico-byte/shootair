4d58acb5ef9b132c742433625fc8a7fc1a202e03
Just2Ez <71229038+Just2Ez@users.noreply.github.com>
2024-03-20 21:33:52 +0100

    better movement

--- Assets/Agent.cs
+++ Assets/Agent.cs
@@ -1,16 +1,18 @@
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
-using Unity.MLAgents.Policies;
-using Unity.VisualScripting.Dependencies.Sqlite;
-using UnityEngine.AI;
+using System.Linq;
+using System;
+using Unity.VisualScripting;
+//using Unity.MLAgents.Policies;
+//using Unity.VisualScripting.Dependencies.Sqlite;
+//using UnityEngine.AI;
 
 public class ShootairAgent : Agent
 {
+    // VARIABLES
     Rigidbody2D rBody;
-    BehaviorParameters behaviorParameters;
     AgentSettings agentSettings;
     private Animator anim;
     public GameObject bulletPrefab;
@@ -18,86 +20,233 @@ public class ShootairAgent : Agent
 
     EnvironmentController envController;
 
-    EnvironmentParameters resetParams;    
-    
+    //BehaviorParameters behaviorParameters;
+    //EnvironmentParameters resetParams;    
+
     void Start()
     {
         envController = FindObjectOfType<EnvironmentController>();
         anim = gameObject.GetComponent<Animator>();
     }
-    
+
     public override void Initialize()
     {
         base.Initialize();
         rBody = GetComponent<Rigidbody2D>();
         agentSettings = FindObjectOfType<AgentSettings>();
-        behaviorParameters = gameObject.GetComponent<BehaviorParameters>();
-
-        resetParams = Academy.Instance.EnvironmentParameters;
+        //behaviorParameters = gameObject.GetComponent<BehaviorParameters>();
+        //resetParams = Academy.Instance.EnvironmentParameters;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-         Cursor.visible = false;
-         Cursor.lockState = CursorLockMode.Locked;
- 
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
         // Check for space key
-        switch (Input.GetKey(KeyCode.Space))
-        {
-            case true:
-                anim.SetBool("aiming", true);
-                break;
-            default:
-                anim.SetBool("aiming", false);
-                break;
-        }
+        bool spacePressed = Input.GetKey(KeyCode.Space);
+
 
         // Check for directional keys
         bool walkingUp = Input.GetKey(KeyCode.W);
         bool walkingDown = Input.GetKey(KeyCode.S);
         bool walkingLeft = Input.GetKey(KeyCode.A);
         bool walkingRight = Input.GetKey(KeyCode.D);
+        bool upArrow = Input.GetKey(KeyCode.UpArrow);
+        bool downArrow = Input.GetKey(KeyCode.DownArrow);
+        bool leftArrow = Input.GetKey(KeyCode.LeftArrow);
+        bool rightArrow = Input.GetKey(KeyCode.RightArrow);
+        bool[] speedCheck = { walkingUp, walkingDown, walkingLeft, walkingRight };
+        bool[] shootCheck = { upArrow, downArrow, leftArrow, rightArrow };
+        int amountSpeed = speedCheck.Count(c => c);
+        int amountShoot = shootCheck.Count(c => c);
 
-        // Set animation bools based on directional keys
-        switch (walkingUp)
+        // MOVEMENT Animation
+        if (amountShoot == 0)
         {
-            case var value when value == true:
+            switch (walkingUp)
+            {
+                case true:
+                    anim.SetBool("walkingUp", true);
+                    break;
+                default:
+                    anim.SetBool("walkingUp", false);
+                    break;
+            }
+            switch (walkingDown)
+            {
+                case true:
+                    anim.SetBool("walkingDown", true);
+                    break;
+                default:
+                    anim.SetBool("walkingDown", false);
+                    break;
+            }
+            switch (walkingLeft)
+            {
+                case true:
+                    anim.SetBool("walkingLeft", true);
+                    break;
+                default:
+                    anim.SetBool("walkingLeft", false);
+                    break;
+            }
+            switch (walkingRight)
+            {
+                case true:
+                    anim.SetBool("walkingRight", true);
+                    break;
+                default:
+                    anim.SetBool("walkingRight", false);
+                    break;
+            }
+        }
+
+        // MOVEMENT
+        float forwardAmount = 0f;
+        float turnAmount = 0f;
+
+        if (walkingUp)
+        {
+            switch (transform.eulerAngles.z)
+            {
+                case 0:
+                case 180:
+                    forwardAmount = transform.eulerAngles.z == 0 ? 1f : -1f;
+                    break;
+                case 90:
+                case 270:
+                    turnAmount = transform.eulerAngles.z == 90 ? 1f : -1f;
+                    break;
+                default:
+                    forwardAmount = 1f;
+                    break;
+            }
+        }
+        if (walkingDown)
+        {
+            switch (transform.eulerAngles.z)
+            {
+                case 0:
+                case 180:
+                    forwardAmount = transform.eulerAngles.z == 0 ? -1f : 1f;
+                    break;
+                case 90:
+                case 270:
+                    turnAmount = transform.eulerAngles.z == 90 ? -1f : 1f;
+                    break;
+                default:
+                    forwardAmount = -1f;
+                    break;
+            }
+        }
+        if (walkingLeft)
+        {
+            switch (transform.eulerAngles.z)
+            {
+                case 0:
+                case 180:
+                    turnAmount = transform.eulerAngles.z == 0 ? -1f : 1f;
+                    break;
+                case 90:
+                case 270:
+                    forwardAmount = transform.eulerAngles.z == 90 ? 1f : -1f;
+                    break;
+                default:
+                    turnAmount = -1f;
+                    break;
+            }
+        }
+        if (walkingRight)
+        {
+            switch (transform.eulerAngles.z)
+            {
+                case 0:
+                case 180:
+                    turnAmount = transform.eulerAngles.z == 0 ? 1f : -1f;
+                    break;
+                case 90:
+                case 270:
+                    forwardAmount = transform.eulerAngles.z == 90 ? -1f : 1f;
+                    break;
+                default:
+                    turnAmount = 1f;
+                    break;
+            }
+        }
+
+        // SHOOTING
+        switch (upArrow)
+        {
+            case true:
                 anim.SetBool("walkingUp", true);
-                break;
-            default:
-                anim.SetBool("walkingUp", false);
-                break;
-        }
-
-        switch (walkingDown)
-        {
-            case var value when value == true:
-                anim.SetBool("walkingDown", true);
-                break;
-            default:
                 anim.SetBool("walkingDown", false);
-                break;
-        }
-
-        switch (walkingLeft)
-        {
-            case var value when value == true:
-                anim.SetBool("walkingLeft", true);
-                break;
-            default:
                 anim.SetBool("walkingLeft", false);
+                anim.SetBool("walkingRight", false);
+                firingPoint.transform.rotation = Quaternion.Euler(0, 0, 0f);
+                break;
+        }
+        switch (downArrow)
+        {
+            case true:
+                anim.SetBool("walkingUp", false);
+                anim.SetBool("walkingDown", true);
+                anim.SetBool("walkingLeft", false);
+                anim.SetBool("walkingRight", false);
+                firingPoint.transform.rotation = Quaternion.Euler(0, 0, 180f);
+                break;
+        }
+        switch (leftArrow)
+        {
+            case true:
+                anim.SetBool("walkingUp", false);
+                anim.SetBool("walkingDown", false);
+                anim.SetBool("walkingLeft", true);
+                anim.SetBool("walkingRight", false);
+                firingPoint.transform.rotation = Quaternion.Euler(0, 0, 90f);
+                break;
+        }
+        switch (rightArrow)
+        {
+            case true:
+                anim.SetBool("walkingUp", false);
+                anim.SetBool("walkingDown", false);
+                anim.SetBool("walkingLeft", false);
+                anim.SetBool("walkingRight", true);
+                firingPoint.transform.rotation = Quaternion.Euler(0, 0, 270f);
                 break;
         }
 
-        switch (walkingRight)
+        if (amountShoot >= 1)
         {
-            case var value when value == true:
-                anim.SetBool("walkingRight", true);
-                break;
-            default:
-                anim.SetBool("walkingRight", false);
-                break;
+            if (agentSettings.fireTimer <= 0f)
+            {
+                Shoot();
+                agentSettings.fireTimer = agentSettings.fireRate;
+            }
+            else
+            {
+                agentSettings.fireTimer -= Time.deltaTime;
+            }
         }
+        else
+        {
+            if (agentSettings.autoShoot && agentSettings.fireTimer <= 0f)
+            {
+                Shoot();
+                agentSettings.fireTimer = agentSettings.fireRate;
+            }
+            else
+            {
+                agentSettings.fireTimer -= Time.deltaTime;
+            }
+        }
+
+        float diagionalSpeed = amountSpeed > 1 ? 0.707106781f : 1f;
+        Vector3 movementForward = agentSettings.moveSpeed * forwardAmount * Time.fixedDeltaTime * transform.up * diagionalSpeed;
+        Vector3 movementTurn = agentSettings.moveSpeed * turnAmount * Time.fixedDeltaTime * transform.right * diagionalSpeed;
+        Vector3 movement = movementForward + movementTurn;
+        rBody.MovePosition(transform.position + movement);
     }
 
     void OnCollisionEnter2D(Collision2D c)
@@ -112,7 +261,7 @@ public class ShootairAgent : Agent
     {
         // Agent position
         sensor.AddObservation(this.transform.localPosition);
-        
+
         // Agent rotation
         sensor.AddObservation(this.transform.rotation);
 
@@ -129,9 +278,10 @@ public class ShootairAgent : Agent
     {
         float forwardAmount = 0f;
         float turnAmount = 0f;
-        
+
+
         if (actionBuffers.DiscreteActions[0] == 1f)
-        {   
+        {
             if (actionBuffers.ContinuousActions[0] == 1 && (transform.eulerAngles.z == 0 || transform.eulerAngles.z == 180))
             {
                 anim.SetBool("aiming", true);
@@ -150,7 +300,7 @@ public class ShootairAgent : Agent
             }
         }
         else if (actionBuffers.DiscreteActions[0] == 2f)
-        {            
+        {
             if (actionBuffers.ContinuousActions[0] == 1 && (transform.eulerAngles.z == 0 || transform.eulerAngles.z == 180))
             {
                 anim.SetBool("aiming", true);
@@ -186,7 +336,7 @@ public class ShootairAgent : Agent
             }
         }
         else if (actionBuffers.DiscreteActions[1] == 2f)
-        {   
+        {
             if (actionBuffers.ContinuousActions[0] == 1 && (transform.eulerAngles.z == 0 || transform.eulerAngles.z == 180))
             {
                 turnAmount = transform.eulerAngles.z == 0 ? 1f : -1f;
@@ -207,12 +357,12 @@ public class ShootairAgent : Agent
         Vector3 movementTurn = transform.right * turnAmount * agentSettings.moveSpeed * Time.fixedDeltaTime;
         Vector3 movement = movementForward + movementTurn;
         rBody.MovePosition(transform.position + movement);
-        
+
         if ((actionBuffers.ContinuousActions[0] == 1 && agentSettings.fireTimer <= 0f) || (agentSettings.autoShoot && agentSettings.fireTimer <= 0f))
         {
             Shoot();
             agentSettings.fireTimer = agentSettings.fireRate;
-        } 
+        }
         else
         {
             agentSettings.fireTimer -= Time.deltaTime;
@@ -221,7 +371,7 @@ public class ShootairAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actionBuffers)
     {
-        MoveAgent(actionBuffers);
+        //MoveAgent(actionBuffers);
     }
 
     private void Shoot()
@@ -234,7 +384,7 @@ public class ShootairAgent : Agent
     {
         int forwardAction = 0;
         int turnAction = 0;
-        
+
         if (Input.GetKey(KeyCode.W))
         {
             // move forward
@@ -242,10 +392,10 @@ public class ShootairAgent : Agent
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            // move forward
+            // move backwards
             forwardAction = 2;
         }
-        
+
         if (Input.GetKey(KeyCode.A))
         {
             // turn left
@@ -260,7 +410,7 @@ public class ShootairAgent : Agent
         // Put the actions into the array
         actionsOut.DiscreteActions.Array[0] = forwardAction;
         actionsOut.DiscreteActions.Array[1] = turnAction;
-        
+
         actionsOut.ContinuousActions.Array[0] = Input.GetKey(KeyCode.Space) ? 1f : 0f;
     }
 }
