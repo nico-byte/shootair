0f543bf702790f869a10e63f70a531b6489682dc
D <daviddeml@gmx.de>
2024-06-09 11:06:22 +0200

    fixing enemy animations

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,3 @@
-using System.Drawing;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
@@ -9,40 +8,30 @@ namespace ShootAirRLAgent
     {
         private Rigidbody2D rBody;
         private Transform target;
-
-        NavMeshAgent enemy;
-
-        Vector3 direction;
+        private NavMeshAgent enemy;
+        private Vector3 direction;
         private Vector2 currentTarget;
         private Animator anim;
-        bool chasing = false;
-        bool waiting = false;
+        private bool chasing = false;
+        private bool waiting = false;
         public bool inViewCone { get; set; }
-
-        EnvironmentController envController;
+        private EnvironmentController envController;
         public int health { get; set; }
 
         [SerializeField]
         private int enemyType;
 
         private Vector2 originalPosition;
-
         private Vector2 previousPosition;
         private int stuckCounter;
         public Vector2 trackVelocity { get; set; }
 
-        private bool dead = false;
-
         public void Awake()
         {
-            // Get a reference to the agent's transform
             target = GameObject.FindGameObjectWithTag("agent").transform;
-
             enemy = GetComponent<NavMeshAgent>();
             enemy.updateRotation = false;
             enemy.updateUpAxis = false;
-
-            // Get a reference to the FSM (animator)
             anim = gameObject.GetComponent<Animator>();
         }
 
@@ -59,44 +48,46 @@ namespace ShootAirRLAgent
             {
                 health = 30;
             }
-            else
+            else if (enemyType == 3)
             {
                 health = 100;
             }
+            else if (enemyType == 4)
+            {
+                health = 200;
+            }
+            else 
+            {
+                health = 250;
+            }
+
+            // optimizing NavMesh settings
+            enemy.speed = 3.5f; // agent speed
+            enemy.acceleration = 8f; // acceleration speed 
+            enemy.angularSpeed = 120f; // angle speed
+            enemy.stoppingDistance = 0.5f; // distance from target
         }
 
         private void Update()
         {
-            // If chasing get the position of the agent and point towards it
             if (chasing)
             {
                 Chase();
-                rotateEnemy();
             }
-
-            // Unless the enemy is waiting then move
-            if (!waiting && !chasing)
+            else if (!waiting)
             {
-                NavMeshPath path = new NavMeshPath();
-                enemy.CalculatePath(currentTarget, path);
-                enemy.SetPath(path);
-                direction = new UnityEngine.Vector2(transform.position.x, transform.position.y) - originalPosition;
-                rotateEnemy();
-                originalPosition = transform.position;
+                MoveToCurrentTarget();
             }
-
         }
 
         private void FixedUpdate()
         {
-            // Give the values to the FSM (animator)
             float distanceFromTarget = Vector2.Distance(currentTarget, transform.position);
             anim.SetFloat("distanceFromWaypoint", distanceFromTarget);
             anim.SetBool("playerInSight", inViewCone);
 
             trackVelocity = (rBody.position - previousPosition) * 50;
 
-            // Check if the enemy is stuck
             if (Vector3.Distance(transform.position, previousPosition) < 1f)
             {
                 stuckCounter++;
@@ -106,7 +97,6 @@ namespace ShootAirRLAgent
                 stuckCounter = 0;
             }
 
-            // Update previousPosition for the next frame
             previousPosition = rBody.position;
 
             if (stuckCounter > 100)
@@ -118,8 +108,6 @@ namespace ShootAirRLAgent
 
         public void SetNextPoint()
         {
-            // Pick a random waypoint 
-            // But make sure it is not the same as the last one
             Vector2 nextPoint;
             Vector2 targetPosition = new Vector2(target.transform.position.x, target.transform.position.y);
 
@@ -130,66 +118,106 @@ namespace ShootAirRLAgent
             while (nextPoint == currentTarget);
 
             currentTarget = nextPoint;
-
-            direction = currentTarget - new Vector2(transform.position.x, transform.position.y);
-            rotateEnemy();
+            MoveToCurrentTarget();
         }
 
         private Vector2 RandomPointInAnnulus(Vector2 center, float minRadius, float maxRadius)
         {
-            Vector2 randomDirection = Random.insideUnitCircle.normalized; // There are more efficient ways, but well
+            Vector2 randomDirection = Random.insideUnitCircle.normalized;
             float minRadius2 = minRadius * minRadius;
             float maxRadius2 = maxRadius * maxRadius;
             float randomDistance = Mathf.Sqrt(Random.value * (maxRadius2 - minRadius2) + minRadius2);
             Vector2 point = center + randomDirection * randomDistance;
-            
+
             return point;
         }
-    
+
         public void Chase()
         {
-            // Load the direction of the player
-            NavMeshPath path = new NavMeshPath();
-            enemy.CalculatePath(target.position, path);
-            enemy.SetPath(path);
-            direction = target.position - transform.position;
-            rotateEnemy();
+            enemy.SetDestination(target.position);
+            UpdateAnimation(enemy.velocity.normalized);
         }
-    
+
         public void StopChasing()
         {
             chasing = false;
         }
 
-        private void rotateEnemy()
-        {   
-            direction = direction.normalized;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            Quaternion lookRotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5);
-
+        public void MoveToCurrentTarget()
+        {
+            enemy.SetDestination(currentTarget);
+            UpdateAnimation(enemy.velocity.normalized);
         }
-    
+
+        void UpdateAnimation(Vector3 direction)
+        {
+            float horizontal = direction.x;
+            float vertical = direction.y;
+
+            // initialising parameters for movement
+            float xMove = 0;
+            float yMove = 0;
+
+            // define main direction
+            if (Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+            {
+                if (horizontal > 0)
+                {
+                    // Enemy movement "right"
+                    xMove = 1.1f;
+                    yMove = 0;
+                }
+                else 
+                {
+                    // Enemy movement "left"
+                    xMove = -1.1f;
+                    yMove = 0;
+                }
+                   
+            }
+            else
+            {
+                if (vertical > 0)
+                {
+                    // Enemy movement "up"
+                    xMove = 0;
+                    yMove = 1.1f;
+                }
+                else
+                {
+                    // Enemy movement "down"
+                    xMove = 0;
+                    yMove = -1.1f;
+                }
+            }
+
+            // setting animation parameters
+            anim.SetFloat("xMove", xMove);
+            anim.SetFloat("yMove", yMove);
+
+            // Static enemy angle
+            float angle = Mathf.Atan2(vertical, horizontal) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, 0); // enemy stays in right angle
+        }
+
         public void StartChasing()
         {
             chasing = true;
         }
-    
-    
+
         public void ToggleWaiting()
         {
-            waiting  = !waiting;
+            waiting = !waiting;
         }
-    
-    	public void TakeDamage (int damage)
-    	{
-    		health -= damage;
-    		if (health <= 0 && !dead)
-    		{
-                dead = true;
-    			Destroy(gameObject);
-    			envController.ResolveEvent(Event.killedTarget);
-    		}
-    	}
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health <= 0)
+            {
+                Destroy(gameObject);
+                envController.ResolveEvent(Event.killedTarget);
+            }
+        }
     }
 }
